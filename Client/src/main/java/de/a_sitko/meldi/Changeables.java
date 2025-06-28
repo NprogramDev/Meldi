00704a9e1719bb0c7fcaf69484e80829b38c1590
NprogramDev <nprogramdev@gmail.com>
2025-06-28 15:23:43 +0200

    fix: UUID not final

--- Client/src/main/java/de/a_sitko/meldi/Changeables.java
+++ Client/src/main/java/de/a_sitko/meldi/Changeables.java
@@ -4,5 +4,5 @@ import java.util.UUID;
 
 public class Changeables {
     public static final String BASE_URL = "https://meldi.nprogramdev.com";
-    public static final String USER_UUID = "c065fa0d-a74c-4723-8fb3-a0eea0d8f17b-f2ef7f77-aa06-4aea-b36b-767dd2886df9";
+    public static String USER_UUID = "c065fa0d-a74c-4723-8fb3-a0eea0d8f17b-f2ef7f77-aa06-4aea-b36b-767dd2886df9";
 }
